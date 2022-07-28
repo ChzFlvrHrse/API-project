d6f87002fd45c9a01a85cdf516313d9f76234f42
Nathan Scott <nate@Nates-PC.localdomain>
2022-07-28 17:26:56 -0500

    Add User authentication Express middleware

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -86,7 +86,7 @@ module.exports = (sequelize, DataTypes) => {
         exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
       }
     },
-    scope: {
+    scopes: {
       currentUser: {
         attributes: { exclude: ["hashedPassword"] }
       },
