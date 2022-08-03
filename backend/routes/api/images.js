bc034fe5c85bc3f5d89991d0bc0d1ed688f02675
Nathan Scott <nate@Nates-PC.localdomain>
2022-08-03 18:29:49 -0500

    image deletion

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -3,12 +3,33 @@ const router = express.Router();
 
 const { Image } = require('../../db/models');
 
+
 router.delete('/:imageId', async (req, res) => {
   const { imageId } = req.params;
   const { user } = req;
   const currUserId = user.dataValues.id;
 
-  const findImage = await Image.findOne()
+  const findImage = await Image.findByPk(imageId);
+
+  if (findImage) {
+    if (findImage.imageableId === currUserId) {
+      await findImage.destroy();
+      res.json({
+        "message": "Successfully deleted",
+        "statusCode": 200
+      })
+    } else {
+      res.json({
+        "message": "Only the owner of this phote can delete it",
+        "statusCode": 400
+      })
+    }
+  } else {
+    res.json({
+      "message": "Image couldn't be found",
+      "statusCode": 404
+    })
+  }
 })
 
 module.exports = router;
