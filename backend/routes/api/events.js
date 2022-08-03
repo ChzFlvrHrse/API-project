8f14d68dff686b659cbf157b5090928425051718
Nathan Scott <nate@Nates-PC.localdomain>
2022-08-03 17:29:59 -0500

    update membership status

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -149,4 +149,80 @@ router.get('/:eventId/attendees', async (req, res) => {
   }
 });
 
+router.post('/:eventId/attendees', async (req, res) => {
+  const { eventId } = req.params;
+  const { user } = req;
+  const userId = user.dataValues.id;
+
+  const findEvent = await Event.findByPk(eventId);
+  const userEvent = await User.findByPk(userId, {
+    include: [{ model: Attendance, attributes: ['status'], where: { eventId } }]
+  })
+
+  if (findEvent) {
+    if (!userEvent) {
+      const newAttendee = await Attendance.create({ eventId, userId, status: 'pending' });
+      res.json(newAttendee)
+    } else if (userEvent.Attendances[0].status === 'pending') {
+      res.json({
+        "message": "Attendance has already been requested",
+        "statusCode": 400
+      })
+    } else if (userEvent.Attendances[0].status !== 'pending') {
+      res.json({
+        "message": "User is already an attendee of the event",
+        "statusCode": 400
+      })
+    }
+  } else {
+    res.json({
+      "message": "Event couldn't be found",
+      "statusCode": 404
+    })
+  }
+});
+
+router.put('/:eventId/attendees', async (req, res) => {
+  const { eventId } = req.params;
+  const { userId, status } = req.body;
+
+  const findEvent = await Event.findByPk(eventId);
+  const userEvent = await User.findAll({
+    include: [{ model: Attendance, where: { eventId } }]
+  });
+
+
+  let isAtt;
+  for (let att of userEvent) {
+    if (att.id === userId) {
+      isAtt = true;
+    }
+  }
+
+  if (findEvent) {
+    if (status === 'pending') {
+      res.json({
+        "message": "Cannot change an attendance status to pending",
+        "statusCode": 400
+      })
+    } else if (!isAtt) {
+      res.json({
+        "message": "Attendance between the user and the event does not exist",
+        "statusCode": 404
+      })
+    } else {
+      const newStatus = await Attendance.findOne({where: {userId}});
+      newStatus.set({eventId: Number(eventId), userId, status});
+      await newStatus.save()
+
+      res.json(newStatus)
+    }
+  } else {
+    res.json({
+      "message": "Event couldn't be found",
+      "statusCode": 404
+    })
+  }
+});
+
 module.exports = router;
