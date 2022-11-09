70c5afac5b3dc5109a27f1fee5c6e5d6292349d5
Nate Scott <nscott1010@gmail.com>
2022-11-08 19:04:15 -0600

    Render Reversion 2
    Render Reversion 2

--- backend/db/seeders/20220802213755-membership-seed.js
+++ backend/db/seeders/20220802213755-membership-seed.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Memberships', [
+    return queryInterface.bulkInsert("Memberships", [
       {
         memberId: 1,
         groupId: 1,
